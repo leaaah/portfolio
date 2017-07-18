248b4c760984640b466516b21c4559d5483650d1
leaaah <lgmoore23@gmail.com>
2017-07-18 15:24:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changetext() {
+    document.getElementById("demo").innerHTML = "Fun Facts";
+    document.getElementById("facts").innerHTML = "February third is my birthday I do not like to read yet I love to write I won the Character Award for modified volleyball I won the Bill Dalton Humanities Award at my eighth grade moving up ceremony Expressing individuality in projects and assignments is important to me I have never eaten at Taco Bell";
+    document.getElementById("factone").innerHTML = "I have Celiacs disease which means that i am allergic to wheat, gluten, barley, rye, ext"
+    document.getElementById("facttwo").innerHTML = "My name is pronounced LEE-ah, not LAY-ay but I respond to both"
+}
+function changepage() {
+    document.getElementById("demotwo").innerHTML = "Social Media";
+}
