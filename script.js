83516f8071030c4e07785f55d05bbd49498a56ff
leaaah <lgmoore23@gmail.com>
2017-07-18 22:19:19 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function changetext() {
     document.getElementById("demo").innerHTML = "Fun Facts";
-    document.getElementById("facts").innerHTML = "February third is my birthday I do not like to read yet I love to write I won the Character Award for modified volleyball I won the Bill Dalton Humanities Award at my eighth grade moving up ceremony Expressing individuality in projects and assignments is important to me I have never eaten at Taco Bell";
-    document.getElementById("factone").innerHTML = "I have Celiacs disease which means that i am allergic to wheat, gluten, barley, rye, ext"
-    document.getElementById("facttwo").innerHTML = "My name is pronounced LEE-ah, not LAY-ay but I respond to both"
+    document.getElementById("facts").innerHTML = "February third is my birthday";
+    document.getElementById("factone").innerHTML = "I have Celiacs disease which means that I am allergic to wheat gluten barley rye";
+    document.getElementById("facttwo").innerHTML = "My name is pronounced LEE-ah, not LAY-ay but I respond to both";
+    document.getElementById("factthree").innerHTML = "I won the Character Award for modified volleyball";
+    document.getElementById("factfour").innerHTML = "I won the Bill Dalton Humanities Award at my eighth grade moving up ceremony";
+    document.getElementById("factfive").innerHTML = "Expressing individuality in projects and assignments is important to me";
+    document.getElementById("factsix").innerHTML = "I have never eaten at Taco Bell";
 }
 function changepage() {
     document.getElementById("demotwo").innerHTML = "Social Media";
